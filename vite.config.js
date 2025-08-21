03035ba3f5411c1834ee99112ec0ed65b197c2b3
Kultai <kultaiali670@gmail.com>
2025-08-21 11:32:15 +0500

    Deploy site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/enterpriseHR/", // ⚡ имя твоего репозитория на GitHub
+  base: "/enterpriseHR/",
 });
